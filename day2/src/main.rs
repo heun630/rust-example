e0f19fc9d9e5a68a94614bcc5a8b0444b728a547
heun630 <egg6030@gmail.com>
2024-12-23 11:31:06 +0900

    feat: rust study

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
+    /*
+    let my_number:u16 = 8;
+    let second_number: u8 = 10;
+    let third_number = my_number + second_number as u16;
+
     println!("Hello, world!");
+    let first_letter = 'A';
+    let space = ' '; // A space inside ' ' is also a char
+    let other_language_char = 'Ꮔ'; // Thanks to Unicode, other languages like Cherokee display just fine too
+    let cat_face = '😺'; // Emojis are chars too
+    */
+
+    let my_number = 'a' as u8;
+    println!("Hello, world! My number is {}", my_number)
 }
--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,16 +1,23 @@
+use std::mem::size_of;
+
 fn main() {
-    /*
-    let my_number:u16 = 8;
-    let second_number: u8 = 10;
-    let third_number = my_number + second_number as u16;
+    // char vs string
+    // char은 4bytes
 
-    println!("Hello, world!");
-    let first_letter = 'A';
-    let space = ' '; // A space inside ' ' is also a char
-    let other_language_char = 'Ꮔ'; // Thanks to Unicode, other languages like Cherokee display just fine too
-    let cat_face = '😺'; // Emojis are chars too
-    */
+    println!("Size of a char: {} bytes", size_of::<char>()); // 4 bytes
 
-    let my_number = 'a' as u8;
-    println!("Hello, world! My number is {}", my_number)
+    // .len() = length
+    // rust에서는 length는 byte를 얘기함
+    println!("Size of string containing 'a': {}", "a".len()); // .len() gives the size of the string in bytes
+    println!("Size of string containing 'ß': {}", "ßßßßß".len());
+    println!("Size of string containing '国': {}", "国".len());
+    println!("Size of string containing '𓅱': {}", "𓅱".len());
+
+    // 글자 수를 알고 싶을 때?
+    // .chars().count()
+    let slice = "Hello!";
+    println!("Slice is {} bytes and also {} characters.", slice.len(), slice.chars().count());
+
+    let slice2 = "안녕!";
+    println!("Slice is {} bytes and also {} characters.", slice2.len(), slice2.chars().count());
 }
